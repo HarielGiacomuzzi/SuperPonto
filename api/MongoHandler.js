ab19bff169c150fb23eb4bf4b7bf8e751df3ed83
harielGiacomuzzi <hariel.dias@acad.pucrs.br>
2016-12-08 09:20:07 -0200

    documented the MongoHandler file

--- api/MongoHandler.js
+++ api/MongoHandler.js
@@ -10,6 +10,9 @@ mongoose.connect(conURL)
 mongoose.set('debug', true)
 
 module.exports = {
+  // This method, creates a new registry fo the
+  // given parameters (First Name, Last Name, E-mail address, Password)
+  // also on completion runs a callback function passin as parameter the user created OR a error if any.
   insertUser: (fName, lName, mail, pass, constr, callback) => {
     let user = new User({
       firstName: fName,
@@ -28,6 +31,9 @@ module.exports = {
     })
   },
 
+// This method is used to search on the mongo documents for a specific
+// user document, it takes as parameters (E-mail address) and
+// reuns a callback function when it's done, passin as argument the user object OR a error if any
   retrieveUserByemail: (email, callback) => {
     User.find({
       email: email
@@ -40,6 +46,9 @@ module.exports = {
     })
   },
 
+// This method creates a new document on mongo, using the UserEntry schema,
+// it takes as arguments (the date wich will be inserted, the user object wich will be linked with and a callback function
+// that receives as parameters the new entry OR an erro if it is the case)
   insertEntry: (date, user, callback) => {
     let entry = new UserEntry({
       date: date,
@@ -55,6 +64,10 @@ module.exports = {
     })
   },
 
+//This method retrieves a User object from mongo, as parameters
+//it takes the ID of the requested user and a callback function
+//wich receives an argument that is the User document OR an error
+//if this is the case
   retrieveEntryByUser: (userID, callback) => {
     UserEntry.find({
       user: userID
@@ -66,7 +79,10 @@ module.exports = {
       callback(entry)
     })
   },
-    
+
+//The method below update the Date element of a UserEntry document,
+//it takes as arguments the ID of the UserEntry, the new date that will replace the
+//old one and a callback funtion with the edited document OR an error if it is the case
   editEntry: (entryID, date, callback) =>{
       UserEntry.update({
           _id : entryID
@@ -77,25 +93,9 @@ module.exports = {
           if(error){
               callback(new Error('Cannot find the entry requested'))
           }else{
-              callback(entry)        
+              callback(entry)
           }
       });
-//      UserEntry.findOneAndUpdate({
-//          _id : entryID  
-//      },{
-//          date : date
-//      }, (error, entry) => {
-//          if( error ){
-//              callback(new Error('Não foi possivel encontrar o registro'));
-//          }else{
-//              callback(entry);
-//          }
-//      });    
-  } 
+  }
 
 }
-
-
-
-
-
